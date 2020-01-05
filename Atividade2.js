647e9535635188bd125d3bd3d8e9e09b82312f61
ClonicLegend <57482146+ClonicLegend@users.noreply.github.com>
2020-01-05 01:15:05 +0000

    Update Atividade2.js

--- Atividade2.js
+++ Atividade2.js
@@ -1,3 +1,18 @@
-function alerta() {
-  alert("I am an alert box!");
+function cores()
+{
+    var myArray = ['#FFC0CB', '#7FFFD4'];    
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    document.getElementById("myDiv").style.backgroundColor = rand;
 }
+
+function calcular(){
+	var end;
+	var op1=parseInt(document.getElementById("recipient-name").value)
+	var op2=parseInt(document.getElementById("recipient-name2").value)
+	end = op1 * op2;
+	alert(end);
+}
+function alerta() {
+  var siv = document.getElementById("txt").value;
+  alert(siv);
+ }
